d940347db8bdf073a5d2a5d4fd630850128df75b
lxsh-S <kumarlakshya973@gmail.com>
2026-07-05 14:30:06 +0530

    chore:refractoring main.go

--- main.go
+++ main.go
@@ -11,25 +11,17 @@ import (
 
 func main() {
 	rootCmd := &cobra.Command{
-		Use:   "gos",
+		Use:   "gos [projectName]",
 		Short: "Gos is used to create folder structures fast!",
+		Args:  cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Welcome...")
+			projectName := args[0]
+			create.Creatego(projectName)
 		},
 	}
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
-	}
-
-	// Les check if the name is given
-	if len(os.Args) < 2 {
-		fmt.Println("Error: Please specify a project name")
-		fmt.Println("Usage: 'gos <your-project-name>'")
-		return
-	} else { // if name is given then let's call Creatego func in Golang.go
-		projectName := os.Args[1]
-		create.Creatego(projectName)
 	}
 }
